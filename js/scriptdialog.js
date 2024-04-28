aa58795432e1aa32d30d845f3746a07a24fae7e7
LermanRodriguez <118776736+LermanRodriguez@users.noreply.github.com>
2024-04-28 14:41:45 -0500

    ¿quieres ser mi novia?

---
+++ js/scriptdialog.js
@@ -0,0 +1,6 @@
+const modal = document.getElementById ("modal");
+const cerrarModal = document.getElementById ("boton-cerrar");
+
+cerrarModal.addEventListener("click", ()=>{
+    modal.close();
+})
